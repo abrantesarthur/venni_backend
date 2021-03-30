e47e5d210ebae2ef24e5b6dc32b6d9bfaa50635c
Arthur Abrantes <aabrantes@college.harvard.edu>
2021-03-30 18:47:13 -0300

    add tests folder

---
+++ functions/test/ride.test.js
@@ -0,0 +1,20 @@
+// reference: https://firebase.google.com/docs/functions/unit-testing?authuser=1
+// initialize firebase-functions-test
+const test = require("firebase-functions-test")(
+  {
+    databaseURL: "https://venni-rider-test-default-rtdb.firebaseio.com/",
+    storageBucket: "venni-rider-test.appspot.com",
+    projectId: "venni-rider-test",
+  },
+  "../testAdminCredentials.json"
+);
+
+// TODO: mock functions config if necessary
+
+// import functions
+const myFunctions = require("../src/ride.ts");
+
+// wrap functions
+const wrappedWidgets = test.wrap(myFunctions.widgets);
+
+// for reference: https://github.com/firebase/functions-samples/blob/master/quickstarts/uppercase/functions/test/test.online.js
